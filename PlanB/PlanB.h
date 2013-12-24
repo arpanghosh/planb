434a2863c98c669fa7f551d843f93c703d056cb5
Arpan Ghosh <arpanghosh@gmail.com>
2013-12-24 12:31:27 -0800

    Initial Commit

---
+++ PlanB/PlanB.h
@@ -0,0 +1,13 @@
+//
+//  PlanB.h
+//  PlanB
+//
+//  Created by Arpan Ghosh on 12/24/13.
+//  Copyright (c) 2013 Plan B. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlanB : NSObject
+
+@end
---
+++ PlanB/PlanB.m
@@ -0,0 +1,13 @@
+//
+//  PlanB.m
+//  PlanB
+//
+//  Created by Arpan Ghosh on 12/24/13.
+//  Copyright (c) 2013 Plan B. All rights reserved.
+//
+
+#import "PlanB.h"
+
+@implementation PlanB
+
+@end
